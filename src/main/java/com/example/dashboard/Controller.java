1873e5b57620d7cb4fc53a8674326e2d8e072e13
leinad <rivasdnaiel.val@gmail.com>
2023-10-15 15:59:01 -0500

    Added baseline main file Added baseline controller file Added baseline fxml file Added visual container and buttons for item hierarchy Added buttons for use cases Added lines to section off containers

---
+++ src/main/java/com/example/dashboard/Controller.java
@@ -0,0 +1,15 @@
+package com.example.dashboard;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
+public class Controller {
+    @FXML
+    public void SelectItem(){
+
+    }
+
+    @FXML
+    private Label welcomeText;
+
+}
