489032ba30ab861d62d2f85816bd4a563d84bfd0
leinad <rivasdnaiel.val@gmail.com>
2023-11-02 10:16:10 -0500

    Added basline packages for tello drone.

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -3,6 +3,7 @@ module com.example.dashboard {
     requires javafx.fxml;
 
     requires com.dlsc.formsfx;
+    requires java.desktop;
 
     opens com.example.dashboard to javafx.fxml;
     exports com.example.dashboard;
